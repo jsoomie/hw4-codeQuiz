7a4f597748133be28a40e994261f72e612efc260
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-03 21:40:55 -0800

    added timer and increased size of text

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var startBtn = document.querySelector(".start-btn");
 var main = document.querySelector("#main");
 var landing = document.querySelector("#landing");
+var timer = document.querySelector(".timer");
+var timeLeft = 75;
 
 // startBtn.addEventListener('click', function() {
 //     console.log("this has been clicked");
@@ -9,3 +11,21 @@ var landing = document.querySelector("#landing");
 //     main.classList.remove("hidden");
 // })
 
+function setTimer() {
+    var timeInterval = setInterval(function() {
+        timeLeft--;
+        timer.textContent = timeLeft + " seconds remaining!";
+
+        if(timeLeft === 0) {
+            clearInterval(timeInterval);
+            lostMessage();
+        }
+        
+    }, 1000);
+}
+
+function lostMessage() {
+    timer.textContent = "Time is up!";
+}
+
+setTimer();
