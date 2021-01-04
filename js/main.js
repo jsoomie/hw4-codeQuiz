584356c1146a090eca9c1849bacf6f3bb6b76c9d
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-03 20:29:49 -0800

    added landing page button. adds hidden class when clicked to go to quiz page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+var startBtn = document.querySelector(".start-btn");
+var main = document.querySelector("#main");
+var landing = document.querySelector("#landing");
+
+startBtn.addEventListener('click', function() {
+    console.log("this has been clicked");
+    landing.classList.remove("center-flex")
+    landing.classList.add("hidden");
+    main.classList.remove("hidden");
+})
