385d42e5df8b4d9fe9573c799ac2ab9668dcf0bb
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-03 21:01:39 -0800

    temporarily removed landing. set header styling

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ var startBtn = document.querySelector(".start-btn");
 var main = document.querySelector("#main");
 var landing = document.querySelector("#landing");
 
-startBtn.addEventListener('click', function() {
-    console.log("this has been clicked");
-    landing.classList.remove("center-flex")
-    landing.classList.add("hidden");
-    main.classList.remove("hidden");
-})
+// startBtn.addEventListener('click', function() {
+//     console.log("this has been clicked");
+//     landing.classList.remove("center-flex")
+//     landing.classList.add("hidden");
+//     main.classList.remove("hidden");
+// })
+
