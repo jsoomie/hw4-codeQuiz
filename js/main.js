6bdfc91a3c6549caac6328fcf05da80c8d1e388e
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-04 00:08:08 -0800

    added questions for the quiz to work

--- js/main.js
+++ js/main.js
@@ -6,14 +6,71 @@
 
 // Element Selector
 elsel = (x) => document.querySelector(x);
+elselall = (x) => document.querySelectorAll(x);
 
 var startBtn = elsel(".start-btn");
 var main = elsel("#main");
 var landing = elsel("#landing");
 var timer = elsel(".timer");
+var questions = elsel(".questions-display");
+var choices = Array.from(elselall(".choice-text"));
+
 var timeLeft = 75;
 
-var dispalyQuestions = document
+var currentQuestion = {};
+var currentPlay = true;
+
+var questionsList = [
+    {
+        question: "The main page of a website is usually referred to as _______________",
+        choice1: "main.html",
+        choice2: "index.html",
+        choice3: "webpage.html",
+        choice4: "landing.html",
+        answer: 2
+    },
+    {
+        question: "How do you print 'Hello World' in console? with javascript?",
+        choice1: "print('Hello World')",
+        choice2: "std::cout >> 'Hello World'",
+        choice3: "System.out.println('Hello World')",
+        choice4: "console.log('Hello World')",
+        answer: 4
+    },
+    {
+        question: "Which main language is used in tandem with HTML and CSS?",
+        choice1: "Javascript",
+        choice2: "PHP",
+        choice3: "JAVA",
+        choice4: "C++",
+        answer: 1
+    },
+    {
+        question: "Which tag is used to link the javascript file to the HTML?",
+        choice1: "<link>",
+        choice2: "<script>",
+        choice3: "<href>",
+        choice4: "<javascript>",
+        answer: 2
+    },
+    {
+        question: "What is Bootstrap in relation to web development?",
+        choice1: "A different language for web development",
+        choice2: "A device used to strap down your boots",
+        choice3: "A collective framework",
+        choice4: "Foundation's newly developed API",
+        answer: 3
+    },
+    {
+        question: "What is the correct syntax to link your CSS file?",
+        choice1: "<link href='./css.style>'",
+        choice2: "<link src='./css.style>'",
+        choice3: "<link link='./css.style>'",
+        choice4: "<link style='./css.style>'",
+        answer: 1
+    },
+]
+
 
 // startBtn.addEventListener('click', function() {
 //     console.log("this has been clicked");
